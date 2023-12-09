b565c3fbac82c549b3bb41d8e1b7df31364b8011
isuru@echonlabs.com <isuru@echonlabs.com>
2023-12-09 13:24:36 +0530

    singUp done

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -7,7 +7,9 @@ export const signup = async (req, res, next) => {
   const newUser = new User({ username, email, password: hashedPassword });
   try {
     await newUser.save();
-    res.status(201).send("user create successfully");
+    res
+      .status(201)
+      .json({ success: true, message: "user create successfully" });
   } catch (error) {
     next(error);
   }
