10938387525aa6edc628860248c36cc612a6103f
isuru@echonlabs.com <isuru@echonlabs.com>
2023-12-12 01:23:53 +0530

    created signIn api & route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -10,6 +12,26 @@ export const signup = async (req, res, next) => {
     res
       .status(201)
       .json({ success: true, message: "user create successfully" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signIn = async (req, res, next) => {
+  //get data from req
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User not found!"));
+    const vaildPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!vaildPassword)
+      return next(errorHandler(401, "Please check the password"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
   } catch (error) {
     next(error);
   }
