e1aca1f500034d754290f3e04f5774bbca90b1ae
IsuruRajithaGamage <isuru.rajitha.gamage@gmail.com>
2024-02-16 20:46:58 +0530

    search api created

--- api/routes/listning.routes.js
+++ api/routes/listning.routes.js
@@ -5,6 +5,7 @@ import {
   deleteListning,
   editListing,
   getListing,
+  getListings,
 } from "../controllers/listning.controller.js";
 
 const router = express.Router();
@@ -13,5 +14,6 @@ router.post("/create", verifyToken, createListning);
 router.delete("/delete/:id", verifyToken, deleteListning);
 router.post("/update/:id", verifyToken, editListing);
 router.get("/get/:id", getListing);
+router.get("/get", getListings);
 
 export default router;
